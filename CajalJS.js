bd9d8b5493c2a90743738c49a6f0ec4742da8836
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2018-03-03 19:13:31 -0800

    update

--- CajalJS.js
+++ CajalJS.js
@@ -3,29 +3,33 @@ function cNet(numOfInputNodes, numOfHiddenNodes, numOfOutputNodes, desiredLearni
     this.hiddenNodes = numOfHiddenNodes;
     this.outputNodes = numOfOutputNodes;
     this.learningRate = desiredLearningRate;
-
     
-    this.name = function() {return this.firstName + " " + this.lastName;};
+    this.wih = randomNormalMatrix(0, Math.pow(numOfHiddenNodes, -0.5), numOfHiddenNodes, numOfInputNodes);
+    this.who = randomNormalMatrix(0, Math.pow(numOfHiddenNodes, -0.5), numOfOutputNodes, numOfHiddenNodes);
+
+    this.train = function(inputs_list, targets_list) {
+      hidden_inputs = math.multiply(this.wih, inputs);
+      hidden_outputs = 
+      final_inputs = math.multiply(this.who, hidden_outputs);
+      final_outputs = 
+
+      hidden_errors = math.multiply(math.transpose(this.who), output_errors);
+      this.who = math.add(this.who, this.learningRate * math.multiply((output_errors * final_outputs * (1.0 - final_outputs)), math.transpose(hidden_outputs)));
+      this.wih = math.add(this.wih, this.learningRate * math.multiply((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), math.transpose(inputs)));
+    }
+
+    this.query = function(inputs_list) {
+      hidden_inputs = math.multiply(this.wih, inputs);
+      final_inputs = math.multiply(this.who, hidden_outputs);
+
+      return final_outputs;
+    }
 }
-
-var rndArray = new Array(m);
-for (i = 0; i < m; i++) {
-  rndArray[i] = new Array(n);
-  for (j = 0; j < n; j++) {
-    rndArray[i][j] = '[' + i + ', ' + j + ']';
-  }
-}
-console.log(rndArray);
-
-randomNormalDistribution(0, 1, 1000000);
-
-/* Random Normal Number Generator
-   Algorithm courtesy of Rao, et al
-   http://interstat.statjournals.net/YEAR/2012/articles/1205003.pdf */
-
-var rndArray = [];
 
 function sampleRandomNormalDistribution(mean, stdDeviation) {
+  /* Random Normal Number Generator
+   Algorithm courtesy of Rao, et al
+   http://interstat.statjournals.net/YEAR/2012/articles/1205003.pdf */
 	var u = Math.random();
 	var z = -Math.log(1 / u - 1) / 1.702;
     z = z * stdDeviation + mean;
