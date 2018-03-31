4da1289272eccc551c4b454117728e2293431efc
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2018-03-31 08:19:16 -0700

    Update check boxes functionality

--- js/CajalJS-Frontend.js
+++ js/CajalJS-Frontend.js
@@ -1,26 +1,28 @@
 
+function CajalJSFrontend() {
+
     function handleFiles(files) {
-      // Check for the various File API support.
-      if (window.FileReader) {
-          // FileReader API is supported.
-          getAsText(files[0]);
-      } else {
-          alert('The FileReader API is not supported in this browser.');
-      }
+        // Check for the various File API support.
+        if (window.FileReader) {
+            // FileReader API is supported.
+            getAsText(files[0]);
+        } else {
+            alert('The FileReader API is not supported in this browser.');
+        }
     }
 
     function getAsText(fileToRead) {
-      var reader = new FileReader();
-      // Read file into memory as UTF-8      
-      reader.readAsText(fileToRead);
-      // Handle load errors
-      reader.onload = loadHandler;
-      reader.onerror = errorHandler;
+        var reader = new FileReader();
+        // Read file into memory as UTF-8      
+        reader.readAsText(fileToRead);
+        // Handle load errors
+        reader.onload = loadHandler;
+        reader.onerror = errorHandler;
     }
 
     function loadHandler(event) {
-      var csv = event.target.result;
-      processData(csv);
+        var csv = event.target.result;
+        processData(csv);
     }
 
     function processData(csv) {
@@ -28,42 +30,42 @@
         var dataColumns;
         var allTextLines = csv.split(/\r\n|\n/);
         var lines = [];
-        for (var i=0; i < allTextLines.length; i++) {
+        for (var i = 0; i < allTextLines.length; i++) {
             var data = allTextLines[i].split(',');
-                var tarr = [];
-                for (var j=0; j < data.length; j++) {
-                    tarr.push(Number(data[j]));
-                }
-                lines.push(tarr);
+            var tarr = [];
+            for (var j = 0; j < data.length; j++) {
+                tarr.push(Number(data[j]));
+            }
+            lines.push(tarr);
         }
 
-console.log(lines);
-console.log(lines[0].length);
-      dataRows = Number(lines.length);
-      if (lines[lines.length - 1].length < lines[0].length) dataRows--;
-      console.log(dataRows);
-      $(".rowsDetected").append(" " + dataRows + " <input type='text' name='FirstName' value='" + dataRows + "'>").hide().fadeIn(1000);
-      $(".columnsDetected").append(" " + lines[0].length + " <input type='text' name='FirstName' value='" + lines[0].length + "'>").hide().fadeIn(1000);
-      $(".targetsColumn").append(" <input type='text' name='targetsCol' value='1'>").hide().fadeIn(1000);
-      $(".xPixels").append(" <input type='text' name='xPix' class='xPix' value='" + Math.sqrt(lines[0].length - 1) + "' disabled>").hide().fadeIn(1000);
-      $(".yPixels").append(" <input type='text' name='yPix' class='yPix' value='" + Math.sqrt(lines[0].length - 1) + "' disabled>").hide().fadeIn(1000);
-      $(".train").prop('disabled', false);
-      $('#checkBox').change(function() {
-        if ($('#checkBox').is(':checked') == true){
-          $('.xPix').prop('disabled', false);
-          $('.yPix').prop('disabled', false);
-       } else {
-         $('.xPix').prop('disabled', true);
-         $('.yPix').prop('disabled', true);
-       }
-    
-    });
+        console.log(lines);
+        console.log(lines[0].length);
+        dataRows = Number(lines.length);
+        if (lines[lines.length - 1].length < lines[0].length) dataRows--;
+        console.log(dataRows);
+        $(".rowsDetected").append(" " + dataRows + " <input type='text' name='FirstName' value='" + dataRows + "'>").hide().fadeIn(1000);
+        $(".columnsDetected").append(" " + lines[0].length + " <input type='text' name='FirstName' value='" + lines[0].length + "'>").hide().fadeIn(1000);
+        $(".targetsColumn").append(" <input type='text' name='targetsCol' value='1'>").hide().fadeIn(1000);
+        $(".xPixels").append(" <input type='text' name='xPix' class='xPix' value='" + Math.sqrt(lines[0].length - 1) + "' disabled>").hide().fadeIn(1000);
+        $(".yPixels").append(" <input type='text' name='yPix' class='yPix' value='" + Math.sqrt(lines[0].length - 1) + "' disabled>").hide().fadeIn(1000);
+        $(".train").prop('disabled', false);
+        $('#displayImageCheckBox').change(function () {
+            if ($('#displayImageCheckBox').is(':checked') == true) {
+                $('.xPix').prop('disabled', false);
+                $('.yPix').prop('disabled', false);
+            } else {
+                $('.xPix').prop('disabled', true);
+                $('.yPix').prop('disabled', true);
+            }
+
+        });
     }
-    
+
     function errorHandler(evt) {
-      if(evt.target.error.name == "NotReadableError") {
-          alert("Cannot read file !");
-      }
+        if (evt.target.error.name == "NotReadableError") {
+            alert("Cannot read file !");
+        }
     }
 
 
@@ -76,44 +78,45 @@ console.log(lines[0].length);
         $(".train").prop('disabled', true);
         $("#csvFileInput").prop('disabled', true);
         transformData($("#dTransform").val());
-        setInterval(function(){ $(".trainingInProgress").fadeTo(900, 0).fadeTo(900, 1); }, 1000);
+        setInterval(function () { $(".trainingInProgress").fadeTo(900, 0).fadeTo(900, 1); }, 1000);
         console.log(test.data);
     }
 
     function transformData(transform) {
         if ($("#dTransform").val() == "") {
-          transform = "X";
+            transform = "X";
         }
         const compiled = math.compile(transform);
-        const mapFunction = function(X) {
-          const scope = { X: X };
-          return compiled.eval(scope);
+        const mapFunction = function (X) {
+            const scope = { X: X };
+            return compiled.eval(scope);
         };
-        const result = math.map(Y, mapFunction); 
+        const result = math.map(Y, mapFunction);
         console.log(result);
         return result;
-      }
-      
+    }
 
-    
 
-    function drawData (el, sample) {
+
+
+    function drawData(el, sample) {
         var counter = 1;
-        var c=document.getElementById("dPreview");
-        var ctx=c.getContext("2d");
-    
+        var c = document.getElementById("dPreview");
+        var ctx = c.getContext("2d");
+
         var x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-   
-    for (y = 0; y < 28; y++) {
-        for (x = 0; x < 28; x++) {
-            ctx.beginPath();
-            ctx.rect(x1,y1,x2,y2);
-            ctx.fillStyle = "rgb(" + (255 - el[sample][counter]) + "," + (255 - el[sample][counter]) + "," + (255 - el[sample][counter]) + ")";
-            ctx.fill();
-            //console.log(x1);
-            counter++;
-            x1 += 4;
-            x2 += 4;
-         }
+
+        for (y = 0; y < 28; y++) {
+            for (x = 0; x < 28; x++) {
+                ctx.beginPath();
+                ctx.rect(x1, y1, x2, y2);
+                ctx.fillStyle = "rgb(" + (255 - el[sample][counter]) + "," + (255 - el[sample][counter]) + "," + (255 - el[sample][counter]) + ")";
+                ctx.fill();
+                //console.log(x1);
+                counter++;
+                x1 += 4;
+                x2 += 4;
+            }
+        }
     }
 }
